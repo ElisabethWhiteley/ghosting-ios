aae96d0d5dd86d5b6e6387f3a07e628b38ad8bbd
WhiEli <elisabeth.whiteley@sbanken.no>
2020-08-29 17:38:03 +0200

    Initial Commit

---
+++ Shared/GreenEggsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenEggsApp.swift
+//  Shared
+//
+//  Created by Elisabeth Whiteley on 29/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct GreenEggsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
